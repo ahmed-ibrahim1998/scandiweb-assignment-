262893d5af112762bd12f2204b706eec00e78700
Ahmed Ibrahim <ahmedibrahim2621998@gmail.com>
2024-10-21 12:53:40 +0300

    fix some problem

--- views/partials/footer.php
+++
@@ -1,6 +0,0 @@
-    <script src="../../public/assets/js/jquery3.4.1.min.js"></script>
-    <script src="../../public/assets/js/bootstrap.min.js"></script>
-    <script src="../../public/assets/js/main.js"></script>
-    </body>
-
-    </html>
