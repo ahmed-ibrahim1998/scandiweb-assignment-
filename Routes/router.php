52a7de54972e053aaa99dd90d019eae50ee7aefd
Ahmed Ibrahim <ahmedibrahim2621998@gmail.com>
2024-10-23 16:41:13 +0300

    EDIT README FILE

--- Routes/router.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Scandiweb\Product\Router\Route;
-
-Route::get('/', "ProductController@index");
-Route::get('/add-product', "ProductController@create");
-Route::post('/store-product', "ProductController@store");
-Route::post('/mass-delete', "ProductController@delete");
