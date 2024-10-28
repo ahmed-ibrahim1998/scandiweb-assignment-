16f144aed84f7c89d22d296bb4364679c2b2c32f
=ahmedibrahim <=ahmedibrahim2621998@gmail.com>
2024-10-28 14:02:44 +0300

    تحديث

--- databese/product_db.sql
+++ databese/product_db.sql
@@ -14,7 +14,32 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+
+-- Dumping database structure for product_db
+CREATE DATABASE IF NOT EXISTS `product_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `product_db`;
+
+-- Dumping structure for table product_db.products
+CREATE TABLE IF NOT EXISTS `products` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sku` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `product_type` enum('DVD','Book','Furniture') NOT NULL,
+  `size` decimal(10,2) DEFAULT NULL,
+  `weight` decimal(10,2) DEFAULT NULL,
+  `height` decimal(10,2) DEFAULT NULL,
+  `width` decimal(10,2) DEFAULT NULL,
+  `length` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sku` (`sku`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table product_db.products: ~0 rows (approximately)
+INSERT INTO `products` (`id`, `sku`, `name`, `price`, `product_type`, `size`, `weight`, `height`, `width`, `length`) VALUES
+	(4, 'SKU001', 'Product 1', 10.99, 'DVD', 700.00, NULL, NULL, NULL, NULL),
+	(5, 'SKU002', 'Product 2', 5.99, 'Book', NULL, 1.50, NULL, NULL, NULL),
+	(6, 'SKU003', 'Product 3', 50.00, 'Furniture', NULL, NULL, 100.00, 50.00, 75.00);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
